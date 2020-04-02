2de17e0af7abd4ce35604765cac3f8b69829b550
Tremayne Souza <treysouz@gmail.com>
2020-04-02 17:24:36 -0400

    Added social link icons, made various layout changes, added previous and next video buttons.  JSON for video information added.

--- src/App.js
+++ src/App.js
@@ -4,17 +4,43 @@ import "./App.css";
 export default class App extends Component {
   render() {
     return (
-      <main>
-        <section className="grid">
-          <div className="name grid-item">Jasmine Armstrong</div>
-          <div className="next grid-item">next</div>
-          <div className="bigScreen grid-item">video</div>
-          <div className="grid-item"> buffer</div>
-          <div className="grid-item"> buffer</div>
-          <div className="grid-item"> buffer</div>
-          <div className="previous grid-item">previous</div>
-          <div className="social grid-item">social</div>
-          <div className="social grid-item">social</div>
+      <main className="grid">
+        <section className="name gridItem">
+          <h1>Jasmine Armstrong</h1>
+        </section>
+        <section className="nextButton gridItem actionTile">
+          <div className="overlay">
+            <i className="fas fa-step-forward">
+              <p>Next Video</p>
+            </i>
+          </div>
+        </section>
+        <section className="mainTile gridItem actionTile"></section>
+        <section className="previewVideos gridItem actionTile">
+          <div className="overlay"></div>
+        </section>
+        <section className="previousButton gridItem actionTile">
+          <div className="overlay">
+            <i className="fas fa-step-backward">
+              <p>Previous Video</p>
+            </i>
+          </div>
+        </section>
+        <section className="socialLinks gridItem">
+          <i className="far fa-id-badge">
+            <span>About Me</span>
+          </i>
+          <i className="fab fa-linkedin">
+            <span>LinkedIn</span>
+          </i>
+        </section>
+        <section className="socialLinks gridItem">
+          <i className="fab fa-imdb">
+            <span>IMBD</span>
+          </i>
+          <i className="fab fa-soundcloud">
+            <span>Soundcloud</span>
+          </i>
         </section>
       </main>
     );
